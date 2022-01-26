6f6e9f0d0345dc558dba43108dcfd60f4fea61b0
HanaMoon <89266961+Marlinv@users.noreply.github.com>
2022-01-26 14:35:55 +0000

    new 26-01-2022

---
+++ MOD2/use_cases/eje1_login.py
@@ -0,0 +1,22 @@
+# pedir datos
+# validar
+# Registro loging
+
+print ("gmail")
+# declara la variable
+
+print ("tell me your username")
+username = input ()
+print ("tell me password")
+password = input ()
+
+while username != 'marlin':
+  print ("es correcto")
+  username = input ('introduzca el username: ')
+
+while password != '147852':
+  print ("es correcto")
+  password = input ('introduzca el password: ')
+
+print ('contrasena correcta')
+print ("Iniciar")
