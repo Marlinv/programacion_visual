caf18f422cc5deb0ab5b5f9f2aa84d1cb436118c
HanaMoon <89266961+Marlinv@users.noreply.github.com>
2022-01-28 13:13:09 +0000

    new buscador

---
+++ MOD2/use_cases/buscador.py
@@ -0,0 +1,14 @@
+print ("dame tu nombre")
+# digite la variable
+nombre = input ()
+
+print ("busca el nombre de: ")
+
+print ("albert garcia") 
+print ("marlin vasquez") 
+print ("aracelis severino")
+
+for nombre in nombres:
+  print(nombre)
+
+numeros = []
