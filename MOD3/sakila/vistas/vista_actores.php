f07896bc376d327aa01bbe546e1a950e8f739592
HanaMoon <89266961+Marlinv@users.noreply.github.com>
2022-03-04 16:26:42 +0000

    ggggggggg

--- MOD3/sakila/vistas/vista_actores.php
+++ MOD3/sakila/vistas/vista_actores.php
@@ -7,24 +7,45 @@
         <h3> <?php echo $pagina; ?></h3>
 
         <div class="row">
-            <div class="col-6">
-                aqui va el formulario
-            </div>
-        </div>
-
-        <div class="row">
-            <form class="col-4">
-                <div class="input-group mb-3">
-                    <input type="text" name="buscador" class="form-control" placeholder="Buscador">
-                    <button class="btn btn-outline-secondary" type="submit" name="boton-buscar">Buscar</button>
+            <form class="col-6" method="post">
+                <div class="mb-3">
+                    <label for="">sakila</label>
+                    <input type="text" name="first_name" class="form-control">
                 </div>
+            <div class="mb-3">
+                <button name="boton-guardar" class="btn btn-primary">Guardar</button>
+            </div>
             </form>
+            
+            <div class="row">
+                <form class="col-4">
+                    <div class="input-group mb-3">
+                        <input type="text" name="buscador" class="form-control" placeholder="Buscador">
+                        <button class="btn btn-outline-secondary" type="submit" name="boton-buscar">Buscar</button>
+                    </div>
+                </form>
+            
+            <?php if (!empty($error)): ?>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong></strong>
+    <?php echo $error; ?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+    <?php endif; ?>
 
         </div>
         <div class="row">
             <div class="col-12">
                 <table class="table table-dark table-striped">
-                    <thead>
+                <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
                     <tbody>
                         <?php
                         $query = "SELECT*FROM actor";
